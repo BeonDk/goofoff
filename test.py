327af91fdf634400adaa4965fdffc6eb76f7da1e
Phil Lee <knowhows2017@gmail.com>
2022-10-19 07:29:52 +0900

    221019

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
 import os
+from datetime import datetime
+from tkinter import E 
+
 
 windows_user_name = os.path.expanduser('~')
 path_dir = f'{windows_user_name}\\desktop\\test_folder'
 file_list = os.listdir(path_dir)
 file_size = os.path.getsize(path_dir)
-
 print(file_size)
 print(os.getcwd())
-# aaaaa
-#fdsafdsa
+
+a = 'C:/Users/PHIL/Desktop/test_folder/KakaoTalk_20221008_200004359.mp4'
+e = os.path.getmtime(a)
+fileMtime = datetime.fromtimestamp(e)
+# strMtime = datetime.strptime(fileMtime, f'%Y%m%d').date()
+# print(fileMtime)
+
+now = datetime.now()
+
+b = now - fileMtime
+print(b)
+
+# 문제 해결!
+if b.days >= 5:
+    print("참")
